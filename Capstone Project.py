b084b190f64fa870a0b72628ee61c307679ee97d
MTavakoliK <64997911+MTavakoliK@users.noreply.github.com>
2020-10-21 22:46:51 -0400

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This Jupyter Notebook will mainly used for Applied Data Science Capstone Project Week 1 for IBM Data Science Certificate
+# 
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
